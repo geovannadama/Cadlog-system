88a0816672892c1698aeb4c2635e5f1893faba8e
geovannadama <geovannadama9987@gmail.com>
2024-09-11 09:50:52 -0300

    11/09 Incluido arquivo de rotas no index e controladores no routes

---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+// Inclui arquivos de controlador para lidar com diferentes ações
+require 'controllers/AuthController.php'; // Inclui o controlador de autenticação
+require 'controllers/UseController.php'; // Inclui o controlador de usuário
+require 'controllers/DashboardController.php'; // Inclui o controlador de dashboard
+
+
+?>
