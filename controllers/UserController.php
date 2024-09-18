7d87ee40f17c4dd290fe47da129fae3a9ddd4a13
geovannadama <geovannadama9987@gmail.com>
2024-09-18 10:18:03 -0300

    verifica se o formulario de cadastro foi enviado

---
+++ controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+class UserController{
+
+    public function register(){
+        // verificar se a requisição HTTP é do tipo Post(se o formulário foi enviado)
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //coleta os dados enviados pelo formulário e organiza em um array
+            $data = [
+                'nome'=> $_POST['nome'],
+                'email'=> $_POST['email'],
+                'senha'=> password_hash( $_POST['senha'], PASSWORD_DEFAULT),
+                //Criptografa a senha 
+                'perfil'=> $_POST['perfil']
+            ];
+            // Chama o método create do model User para criar o novo usuário no BD
+
+            User::create($data);
+            header('Location: index.php');
+        }else{
+            include 'views/register.php'
+        }
+    }
+}
