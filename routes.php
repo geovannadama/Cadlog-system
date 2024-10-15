7179ed9fde58be6c3d2abaa8075ef60694211150
geovannadama <geovannadama9987@gmail.com>
2024-10-15 11:58:58 -0300

    criação do botão de logout e lista de usuários

--- routes.php
+++ routes.php
@@ -1,22 +1,32 @@
 <?php
 // Inclui arquivos de controlador para lidar com diferentes ações
-require 'controllers/AuthController.php'; // Inclui o controlador de autenticação
-require 'controllers/UseController.php'; // Inclui o controlador de usuário
-require 'controllers/DashboardController.php'; // Inclui o controlador de dashboard
-
+require 'controllers/AuthController.php'; // inclui o controlador de autenticação
+require 'controllers/UserController.php'; // inclui o controlador de usuário
+require 'controllers/DashboardController.php'; // inclui o controlador de dashboard
+ 
 // Cria instâncias dos controladores para utilizar seus métodos
-$authController = new AuthController(); 
-//Intância o controlador de autenticação 
-
+$authController      = new AuthController(); // Instancia o controlador de autenticação
+$userController      = new UserController();
+$dashboardController = new DashboardControler();
+ 
 // Coleta a ação da URL, se não houver definida, usa 'login' por padrão
-$action = $_GET['action'] ?? 'login'; 
-// Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente 
-
-// Verifica a ação solicitada e chama o método apropriado do controlador 
+$action = $_GET['action'] ?? 'login'; // Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
+ 
+// Verifica a ação solicitada e chama o método apropriado do controlador
 switch($action){
     case 'login':
-        $authController-> login(); 
-        // chama o método login do controlador de autenticação
+        $authController->login(); // chama o método login do controlador de autenticação
+        break;
+    case 'register':
+        $userController->register();
+        break;
+    case 'dashboard':
+        $dashboardController->index();
+        break;
+    case 'logout':
+        $authController->logout();
+    default:
+        $authController->login();
+        break;
 }
-
 ?>
